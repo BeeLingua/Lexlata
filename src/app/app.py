1c8e144cc66a61b0fa0a366888b526d15f150842
Eyupcan Cakir <eypcnckr@gmail.com>
2022-08-14 21:51:53 +0300

    'detailed info button activated

--- src/app/app.py
+++ src/app/app.py
@@ -2,13 +2,16 @@ import streamlit as st
 from streamlit.components.v1 import html
 from model import run
 from datetime import datetime
+import locale
+
+locale.setlocale(locale.LC_ALL, 'turkish')
 
 st.set_page_config(
    page_title="Lexlata",
    page_icon="https://media-public.canva.com/RA1JI/MAE0sNRA1JI/1/t.png"
 )
 
-st.markdown("""<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>""", unsafe_allow_html=True)
+st.markdown("""<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">""", unsafe_allow_html=True)
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg bg-light">
@@ -32,10 +35,14 @@ st.markdown("""
 st.markdown(""" <style>
 .css-10trblm {font-family: sans-serif;}
 .css-1v3fvcr {background-color: #2c5e74; background-image: url('https://media-public.canva.com/9KIMs/MAEqpM9KIMs/1/tl.png');}
+.css-13sdm1b {color: #2c5e74;}
 header {visibility: hidden;}
 footer {visibility: hidden;}
 .card {margin-bottom: 40px;color: black;}
-.card-body button {color: #fff;}
+.btn-primary {color: #ddd; background-color: #2c5e74; border-color: #2c5e74;}
+.btn-primary:hover {color: #fff; background-color: #1a3d41; border-color: #1a3d41;}
+.modal-content {color: black;}
+.btn-primary:not(:disabled):not(.disabled).active:focus, .btn-primary:not(:disabled):not(.disabled):active:focus, .show>.btn-primary.dropdown-toggle:focus {box-shadow: 0 0 0 0.2rem rgb(44 94 116 / 50%);}
 </style> """, unsafe_allow_html=True)
 
 st.title('LEXLATA')
@@ -64,29 +71,40 @@ for i in range(len(res)):
   </div>
   <div class="card-body">
     <p class="card-text">{res[i]['name']}</p>
-    <button type="button" class="btn btn-primary" onclick="open({str(i) + '.law'})">İncele</button>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#{str(i) + '.law'}">İncele</button>
   </div>
   <div class="card-footer text-muted">
     {datetime.strptime(res[i]['date'], '%Y-%m-%d').strftime('%d %B %Y') if isinstance(res[i]['date'], str) else 'Belirsiz' }
   </div>
-</div>
-<div class="modal fade" tabindex="-1" id="{str(i) + '.law'}" tabindex="-1" aria-labelledby="{str(i) + '.lawLabel'}" style="display: none;" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+  </div>
+  <div class="modal fade" tabindex="-1" id="{str(i) + '.law'}" tabindex="-1" aria-labelledby="{str(i) + '.lawLabel'}" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{res[i]['name']}</h5>
+          <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>{res[i]['info']}</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Kapat</button>
+          <a type="button" class="btn btn-primary" href="{res[i]['link']}">Kaynağı Görüntüle</a>
+        </div>
       </div>
     </div>
-  </div>
-</div>""")
+  </div>""")
   #<p class="mb-1">Mevzuat Bilgisi: {str(res[i]['info'][0:15]).join('...')}</p>
-js_open = "(function (){console.log(document.getElementById('0.law'));})();"
-html(f"""<div class="list-group">{laws}</div><script>{js_open}</script><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>""")
+st.markdown(f"""<div class="list-group">{laws}</div>""",unsafe_allow_html=True)
+script = """<script>
+    sources = ['https://code.jquery.com/jquery-3.2.1.slim.min.js', 'https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js', 'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js']
+    integrities = ['sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN', 'sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q', 'sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl']
+    for (var i = 0; i < 3; i++) {
+      script = parent.document.createElement('script');
+      script.setAttribute('src', sources[i]);
+      script.setAttribute('integrity', integrities[i]);
+      script.setAttribute('crossorigin', 'anonymous');
+      parent.document.body.appendChild(script);
+    }
+  </script>"""
+html(f"""{script}""",height=0)
